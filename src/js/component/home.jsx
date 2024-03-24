356b47f58f251479c60f8034d33676e620ae33b7
Truitt Janney <153782672+truittjanney@users.noreply.github.com>
2024-03-24 13:28:00 +0000

    project complete

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,38 @@
-import React from "react";
+import React, { useState } from 'react';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
+// New Component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+	const [tasks, setTasks] = useState([]);
+	const [inputValue, setInputValue] = useState("");
+
+const addTask = () => {
+	if(inputValue.trim() !== "") {
+		setTasks([...tasks, inputValue]);
+		setInputValue("");
+	}
 };
+
+const removeTask = (index) => {
+	const updatedTasks = tasks.filter((_, i) => i !== index);
+	setTasks(updatedTasks);
+};
+
+return (
+	<div>
+		<h1>To-Do List:</h1>
+		<input type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)}
+		placeholder="Enter a task..."/>
+		<button onClick={addTask}>Add Task</button>
+		<ul>
+			{tasks.map((task, index) => (
+				<li key={index}>
+					{task}
+					<button onClick={() => removeTask(index)}>Remove</button>
+				</li>
+			))}
+		</ul>
+	</div>
+);
+			};
 
 export default Home;
